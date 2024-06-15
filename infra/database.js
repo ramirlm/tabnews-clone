6ac41750a00aa001c5c82bac2054d3c01055086a
Ramir Mesquita <790844+ramirlm@users.noreply.github.com>
2024-06-15 18:56:54 +0000

    refactor: improving `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,21 @@
-import pg from "pg";
+import { Client } from "pg";
 
-async function database() {
-  const { Client } = pg;
-  const client = new Client();
-  await client.connect();
-  const res = await client.query("SELECT $1::text as message", [
-    "Hello world!",
-  ]);
-  await client.end();
+async function query(queryObject) {
+  const client = new Client({
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRESE_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
-export default database;
+export default query;
