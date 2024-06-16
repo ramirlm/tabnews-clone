c3faede7a4d723c0f01a805693de9a34750631ec
Ramir Mesquita <790844+ramirlm@users.noreply.github.com>
2024-06-16 16:45:33 +0000

    fix: fixing env var typo

--- infra/database.js
+++ infra/database.js
@@ -4,7 +4,7 @@ async function query(queryObject) {
   const client = new Client({
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRESE_USER,
+    user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
 
